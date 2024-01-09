3ab0e53b3746d6edeb6384d1261f01d6fbf0e948
Anuj Kashyap <anujs5761@gmail.com>
2024-01-09 19:36:52 +0530

    Add some new Chapters

---
+++ 35_Table_Navigation _ JavaScript_Tutorial_in_Hindi.js
@@ -0,0 +1,9 @@
+let t = document.body.firstElementChild.firstElementChild
+// console.log(t)
+// console.log(t.rows)
+// console.log(t.tHead)
+// console.log(t.tHead.firstElementChild)
+// console.log(t.caption)
+// console.log(t.tBodies)
+// console.log(t.rows[3].rowIndex)
+console.log(t.tr.cells)
---
+++ 36_Searching_the_DOM _ JavaScript_Tutorial_in_Hindi.js
@@ -0,0 +1,21 @@
+// let ctitle = document.getElementsByClassName("conts2")[0]
+// ctitle.style.color = "blue"
+
+// let ctitle = document.getElementsByClassName("conts2")[0]
+// ctitle.style.height = "10px"
+
+// let ctitle = document.getElementsByClassName("conts2-title")[0]
+// ctitle.style.color = "red"
+
+// let ctitle = document.getElementById("conts2-title")
+// ctitle.style.color = "red"
+
+// let ctitle = document.querySelectorAll(".conts1")
+// ctitle[0].style.color = "red"
+// console.log(ctitle)
+
+// document.querySelector(".conts2").style.color = "red"
+// document.querySelector(".conts2").style.background = "blue"
+
+// console.log(document.getElementsByTagName("div"))
+// console.log(document.querySelector(".container").getElementsByTagName("div"))
