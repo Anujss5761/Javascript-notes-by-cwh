2a45b5d05e3297cd405ec4bbbc87dff3ba16b9d4
Anuj Kashyap <anujs5761@gmail.com>
2024-01-30 19:48:28 +0530

    mm

--- 52_Introduction_to_Callbacks __JavaScript_Tutorial_in_Hindi.js
+++ 52_Introduction_to_Callbacks __JavaScript_Tutorial_in_Hindi.js
@@ -12,23 +12,67 @@
 // },3000)
 // console.log("End")
 
+
+
+
+// function loadScript(src, callback){
+//     var script = document.createElement("script");
+//     script.src = src;
+//     script.onload = function() {
+//         console.log("Loaded script with SRC: " + src);
+//         callback(src);
+//     }
+//     document.body.appendChild(script);
+//     }
+
+// function hello(src){
+//     alert("Hello Bharat" + src)
+// }
+
+
+// function goodmorning(src){
+//     alert("Good Morning" + src)
+// }
+// loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.esm.min.js", hello)
+
+
+
+
+// Error Handling
 function loadScript(src, callback){
     var script = document.createElement("script");
     script.src = src;
     script.onload = function() {
         console.log("Loaded script with SRC: " + src);
-        callback();
-
+        callback(null, src);
+    }
+    script.onerror = function() {
+        console.log("Error loading script with src" + src);
+        callback(new error("SRC got some error"));
     }
     document.body.appendChild(script);
     }
 
-function hello(){
-    alert("Hello Bharat")
+function hello(error, src){
+    // alert("Hello Bharat" + src)
+    if(error){
+        console.log(error);
+        return;
+    }
+    alert("Hello" + src)
 }
 
 
-function goodmorning(){
-    alert("Good Morning")
+function goodmorning(src){
+    if(src){
+        console.log(src);
+        return;
+    }
+    alert("Good Morning" + src)
 }
-loadScript("https://www.amazon.in/Kikibix-Currant-Natural-Transfat-Emulsifiers/dp/B0C9X87YN8/ref=sr_1_1?crid=5BA1RONT7SO4&keywords=B0C9X87YN8&nsdOptOutParam=true&qid=1706461807&sprefix=b0c9x87yn8%2Caps%2C281&sr=8-1", hello)
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.esm.min.js", hello)
+
+
+
+
+
