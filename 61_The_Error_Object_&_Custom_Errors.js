fed136ad0bc2e8d9d19d9498d1b0f30a86177140
Anuj Kashyap <anujs5761@gmail.com>
2024-02-24 00:54:40 +0530

    mm

---
+++ 61_The_Error_Object_&_Custom_Errors.js
@@ -0,0 +1,30 @@
+
+// CustomError
+// try{
+//     console.log(Anuj)
+//     throw new ReferenceError("Anuj is not defined")
+// }
+// catch(error){
+//       console.log(error.name)
+//       console.log(error.message)
+//       console.log(error.stack)
+// }
+
+
+
+
+try{
+    let age = prompt("Enter your age")
+    age = Number.parseInt(age)
+    if(age>150){
+        throw new ReferenceError("This is probably not true")
+    }
+}
+catch(error){
+      console.log(error.name)
+      console.log(error.message)
+      console.log(error.stack)
+}
+
+
+console.log("The script is still running...")
---
+++ 62_The_Finally_Clause.js
@@ -0,0 +1,18 @@
+const f = () => {
+  try {
+    let a = 0;
+    console.log(program);
+    console.log("Program is running successfully");
+    return;
+  } catch (err) {
+    console.log("This is an error");
+    console.log(p);
+  } finally {
+    console.log("I am a good boy.");
+    // Close the file
+    // Exit the loop
+    // Write to the log
+  }
+};
+
+f()
